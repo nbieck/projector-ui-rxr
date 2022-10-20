cc6a67453b08b797ee42325b7d245a9bc7418117
Niklas Bieck <nbieck@uef.fi>
2022-10-20 11:12:18 +0300

    Remove more files that should be ignored

--- workspace/build/usb_cam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/src/usb_cam
-# Build directory: /workspace/build/usb_cam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
