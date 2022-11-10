f24746e0ddb25f55a388b72f15a71049cad6dcb7
shiohiyoko <takagif315@gmail.com>
2022-11-10 13:49:15 +0200

    working

--- src/window.py
+++ src/window.py
@@ -6,22 +6,25 @@ import numpy as np
 width = 640
 height = 480
 
+trans_m = np.array([[1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])
+
+buttons = np.array([[[0.1, 0.25, 0, 1],
+                     [0.40, 0.25, 0, 1],
+                     [0.40, 0.75, 0, 1],
+                     [0.1, 0.75, 0, 1]],
+                     [[0.6, 0.25, 0, 1],
+                     [0.90, 0.25, 0, 1],
+                     [0.90, 0.75, 0, 1],
+                     [0.6, 0.75, 0, 1]]])
 class Window:
-    def __init__(self) -> None:
-        self.trans_m = np.array([[1, 0, 0, 0],
-                                 [0, 1, 0, 0],
-                                 [0, 0, 1, 0],
-                                 [0, 0, 0, 1]])
+    def __init__(self, features, trans_m) -> None:
+        self.trans_m = trans_m
 
-        self.button1 = np.array([[0.1,  0.25, 0, 1],
-                                 [0.40, 0.25, 0, 1],
-                                 [0.40, 0.75, 0, 1],
-                                 [0.1,  0.75, 0, 1]])
+        self.features = features
 
-        self.button2 = np.array([[0.6,  0.25, 0, 1],
-                                 [0.90, 0.25, 0, 1],
-                                 [0.90, 0.75, 0, 1],
-                                 [0.6,  0.75, 0, 1]])
         if not glfw.init():
             raise RuntimeError('Could not initialize GLFW3')
 
@@ -30,52 +33,55 @@ class Window:
         if not self.window:
             glfw.terminate()
             raise RuntimeError('Could not create an window')
+        
+        self.init()
       
     def init(self):
-        glClearColor(0.0, 1.0, 0.0, 1.0)
-        glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0)
-        self.display()   # necessary only on Windows
-    
-    def run(self):
-
         glfw.set_cursor_pos_callback(self.window, self.cursor_pos)
         glfw.set_cursor_enter_callback(self.window, self.cursor_enter)
         glfw.set_mouse_button_callback(self.window, self.mouse_button)
         glfw.set_scroll_callback(self.window, self.scroll)
-        glfw.set_window_refresh_callback(self.window, self.window_refresh)
+        # glfw.set_window_refresh_callback(self.window, self.window_refresh)
         glfw.make_context_current(self.window)
 
-        self.init()
+        glClearColor(0.0, 1.0, 0.0, 1.0)
+        glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0)
+        self.display(self.window)   # necessary only on Windows
+    
+    def run(self):
 
         while not glfw.window_should_close(self.window):
             glfw.wait_events_timeout(1e-3)
+            self.display(self.window)
+            glfw.poll_events()
 
         glfw.terminate()
 
   
-    def draw(self, tool):
+    def draw(self, tool, window):
         glColor3f(1.0, 1.0, 1.0)
-        glBegin(GL_POLYGON)
-        for vt in tool:
-            w = np.dot(self.trans_m, vt.T)
-            glVertex2f(w[0]/w[2],w[1]/w[2])
-        glEnd()
 
-    def display(self):
+        for f in tool:
+            glBegin(GL_POLYGON)
+            for vt in f:
+                w = np.dot(self.trans_m, vt.T)
+                glVertex2f(w[0]/w[-1],w[1]/w[-1])
+            glEnd()
+
+    def display(self,window):
         glClear(GL_COLOR_BUFFER_BIT)
 
-        self.draw(self.button1)
-        self.draw(self.button2)
+        self.draw(self.features, window)
 
-        glfw.swap_buffers(self.window)
+        glfw.swap_buffers(window)
 
     def cursor_pos(self, window, xpos, ypos):
         x = xpos/width
         y = ypos/height
-        if x > self.button1[0][0] and y > self.button1[0][1] and x < self.button1[2][0] and y < self.button1[2][1]:
+        if x > self.features[0][0][0] and y > self.features[0][0][1] and x < self.features[0][2][0] and y < self.features[0][2][1]:
             print("self.button 1 pressed")
 
-        if x > self.button2[0][0] and y > self.button2[0][1] and x < self.button2[2][0] and y < self.button2[2][1]:
+        if x > self.features[1][0][0] and y > self.features[1][0][1] and x < self.features[1][2][0] and y < self.features[1][2][1]:
             print("self.button 2 pressed")
 
 
@@ -107,8 +113,8 @@ class Window:
         print('scroll:', xoffset, yoffset)
 
     def window_refresh(self, window):
-        self.display()
+        self.display(window)
 
 
-windo = Window()
+windo = Window(features=buttons, trans_m=trans_m)
 windo.run()
