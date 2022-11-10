68c6386af9bc57d24182379f6022b6e936c34ce2
Niklas Bieck <nbieck@uef.fi>
2022-11-10 14:40:14 +0200

    Fix calibration inputs. We only need 3 fiducials, but projector fov and aspect ratio must be provided

--- src/calibration.py
+++ src/calibration.py
@@ -1,11 +1,12 @@
 import numpy as np
 import numpy.typing as npt
-from typing import List
-from math_utils import Frustum
+from typing import List, Optional
+from math_utils import Frustum, AngleFormat
 
-def calibrate(world_points: List[npt.NDArray], texture_points: List[npt.NDArray]) -> Frustum:
+def calibrate(world_points: List[npt.NDArray], texture_points: List[npt.NDArray], aspect_ratio: float, fov_format: AngleFormat = AngleFormat.RAD, *,
+              hfov: Optional[float] = None, vfov: Optional[float] = None) -> Frustum:
     """
-        Given 4 points an world space (camera view space) and the corresponsing texture coordinates in 
+        Given 3 points an world space (camera view space) and the corresponsing texture coordinates in 
         the projector's screen space, computes the Frustum of the projector in whatever coordinate system the
         world space points are in. (The solution will only be somewhat approximate, but should be good enough)
         Important, the points should correspond directly, i.e. the first world point is the projection of the first
@@ -15,10 +16,10 @@ def calibrate(world_points: List[npt.NDArray], texture_points: List[npt.NDArray]
          - texture_points: A list of 4 ndarrays of length 2, each representing a point in the screen space of the projector.
                            All coordinate values here should be in the interval [0,1]
     """
-    if len(world_points) != 4:
-        raise ValueError("World Points for calibration should be exactly 4.")
-    if len(texture_points) != 4:
-        raise ValueError("Texture Points for calibration should be exacly 4.")
+    if len(world_points) != 3:
+        raise ValueError("World Points for calibration should be exactly 3.")
+    if len(texture_points) != 3:
+        raise ValueError("Texture Points for calibration should be exacly 3.")
     for p in world_points:
         if p.size != (3,):
             raise ValueError("World Points should have 3 elements")
@@ -28,4 +29,8 @@ def calibrate(world_points: List[npt.NDArray], texture_points: List[npt.NDArray]
         if np.any(p < 0 or p > 1):
             raise ValueError("All Texture Coordinates should be in the range [0,1]")
 
-    return None
+
+
+    raise NotImplementedError("Not fully implemented yet")
+
+
