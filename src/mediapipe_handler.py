b258357a839b966f1437b1e0869bea6538099706
Takehiro Izumi <33809675+sytk@users.noreply.github.com>
2022-11-23 09:29:28 +0900

    :sparkles: add offset for aligned images

--- src/mediapipe_handler.py
+++ src/mediapipe_handler.py
@@ -14,6 +14,12 @@ class MediapipeHandler():
             min_tracking_confidence=0.5)
 
     def detectHands(self, image):
+        idxs = np.where(np.sum(image, axis=2) != 0)
+        self.pixel_min = np.min(idxs, axis=1)
+        self.pixel_max = np.max(idxs, axis=1)
+        image = image[self.pixel_min[0]:self.pixel_max[0],
+                      self.pixel_min[1]:self.pixel_max[1]]
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         self.image_height, self.image_width = image.shape[0:2]
         results = self.hands.process(image)
@@ -24,7 +30,8 @@ class MediapipeHandler():
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 self.mp_drawing.draw_landmarks(
-                    image,
+                    image[self.pixel_min[0]:self.pixel_max[0],
+                          self.pixel_min[1]:self.pixel_max[1]],
                     hand_landmarks,
                     self.mp_hands.HAND_CONNECTIONS,
                     self.mp_drawing_styles.get_default_hand_landmarks_style(),
@@ -39,7 +46,23 @@ class MediapipeHandler():
             z = hand.landmark[8].z
             x = int(np.clip(x * self.image_width, 0, self.image_width-1))
             y = int(np.clip(y * self.image_height, 0, self.image_height-1))
-            return x, y, z
+            return x+self.pixel_min[1], y+self.pixel_min[0], z
+        return None
+
+    def isGestureNeutral(self, results):
+        if results.multi_hand_landmarks:
+            hand = results.multi_hand_landmarks[0]
+            landmarks = np.zeros((21, 3))
+            for i, landmark in enumerate(hand.landmark):
+                landmarks[i][0] = landmark.x
+                landmarks[i][1] = landmark.y
+                landmarks[i][2] = landmark.z
+
+            min_pos = np.min(landmarks, axis=0)
+            max_pos = np.max(landmarks, axis=0)
+            landmarks = (landmarks - min_pos) / (max_pos-min_pos)
+
+            return np.sum((landmarks[4] - landmarks[20]) ** 2)
         return None
 
 
