ca96609d64df38ae85bfd62b88e873b61eca5aac
Takehiro Izumi <33809675+sytk@users.noreply.github.com>
2022-11-23 09:32:57 +0900

    :sparkles: touch detection based on plane detection

--- src/touch_detection.py
+++ src/touch_detection.py
@@ -2,37 +2,77 @@ from mediapipe_handler import MediapipeHandler
 from realsense_handler import RealsenseHandler
 import cv2
 import numpy as np
+import time
+
+
+def calculateDistancePoint2Plane(point, plane):
+    # point: x,y,z
+    # plane: a,b,c,d (ax + by + cz + d = 0)
+    dis = np.abs(np.sum(plane[0:3] * point) +
+                 plane[3]) / np.sqrt(np.sum(plane[0:3]**2))
+    return dis
+
 
 if __name__ == "__main__":
     RSH = RealsenseHandler()
     MPH = MediapipeHandler()
     UI_base_depth = 0
+    t0 = 0
+    count = 30
     try:
         while True:
+            fps = int(1/(time.time() - t0))
+            t0 = time.time()
+
             color_image, depth_image = RSH.getFrames()
             hands = MPH.detectHands(color_image)
             cursor = MPH.getIndexFingerPositions(hands)
-
             image = MPH.drawLandmarks(color_image, hands)
+            print(MPH.isGestureNeutral(hands))
+            cv2.putText(image,
+                        str(fps),
+                        org=(50, 50),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=1.0,
+                        color=(255, 0, 0),
+                        thickness=2,
+                        lineType=cv2.LINE_4
+                        )
+
+            points, texcoords = RSH.getPointCloud()
+
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
+                depth_image, alpha=0.1), cv2.COLORMAP_JET)
+
+            if count >= 30:
+                results = RSH.detectPlanes(
+                    points, texcoords, down_sampling_rate=1/41)
+                count = 0
+            count += 1
 
             h, w = color_image.shape[0:2]
-            left_up = (int(w/4.0), int(h/4.0))
-            right_down = (int(w*3/4.0), int(h*3/4.0))
-            cv2.rectangle(image, left_up, right_down, (255, 0, 0))
+            left_up = (int(w/3.0), int(h/3.0))
+            right_down = (int(w*2/3.0), int(h*2/3.0))
 
-            UI_area_depth = depth_image[left_up[1]
-                :right_down[1], left_up[0]:right_down[0]]
+            cv2.rectangle(image, left_up, right_down, (255, 0, 0))
+            UI_area_depth = depth_image[left_up[1]:right_down[1],
+                                        left_up[0]:right_down[0]]
             UI_depth = np.mean(UI_area_depth[UI_area_depth > 0])
 
             if cursor is not None:
-                cursor_depth = depth_image[cursor[1], cursor[0]]
-                print(UI_depth, cursor_depth)
+                texcoords = np.asarray(
+                    texcoords * np.asarray([w, h]), dtype=np.uint16)
+
+                p = np.argmin(np.sum(np.abs(texcoords - cursor[0:2]), axis=1))
+                cursor_depth = calculateDistancePoint2Plane(
+                    points[p], results[0][0]) * 100  # cm
+                # print(cursor, texcoords[p], points[p], cursor_depth)
 
                 c = (255, 0, 0)
 
-                if UI_base_depth - cursor_depth < 30 \
-                        and left_up[1] < cursor[0] < right_down[1] \
-                        and left_up[0] < cursor[1] < right_down[0]:
+                if cursor_depth < 2 \
+                        and left_up[1] < cursor[1] < right_down[1] \
+                        and left_up[0] < cursor[0] < right_down[0]:
                     c = (0, 0, 255)
 
                 cv2.drawMarker(image, (int(cursor[0]), int(cursor[1])), c, markerType=cv2.MARKER_CROSS,
@@ -40,6 +80,7 @@ if __name__ == "__main__":
             else:
                 UI_base_depth = UI_depth
 
+            image = RSH.drawPlanes(image, results)
             images = RSH.combineImages(image, depth_image)
             cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
             cv2.imshow('Align Example', images)
@@ -49,5 +90,7 @@ if __name__ == "__main__":
                 cv2.destroyAllWindows()
                 break
     finally:
+        print("closing")
+        time.sleep(1)
         del RSH
         del MPH
